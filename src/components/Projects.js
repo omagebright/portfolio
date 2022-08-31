edae5a2d19987a6dc628155959aac861ab75b2a2
omagebright <omagefolorunsho@gmail.com>
2022-08-31 10:12:16 -0300

    Portfolio in React first commit

---
+++ src/components/Projects.js
@@ -0,0 +1,107 @@
+import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import projImg4 from "../assets/img/project-img4.png";
+import projImg5 from "../assets/img/project-img5.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Portfolio page",
+      description: "ReactJS",
+      imgUrl: projImg1,
+      plink: '#home'
+    },
+    {
+      title: "Profile Page",
+      description: "Vanilla JS",
+      imgUrl: projImg2,
+      plink: 'https://omage.tech',
+    },
+    {
+      title: "Startup",
+      description: "WordPress",
+      imgUrl: projImg3,
+      plink: 'https://uicounsel.com/'
+      
+    },
+    {
+      title: "Arxspan Note- Meet Clone",
+      description: "Vanilla JS",
+      imgUrl: projImg4,
+      plink: 'https://elect-notebook.netlify.app/',
+    },
+    {
+      title: "Android App",
+      description: "Dart and Flutter",
+      imgUrl: projImg5,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>Created projects that use AI to analyze data and make decisions, Led and shipped the admin interface for many platforms, built with Backend technologies, Wrote and reviewed code for JS, Express and React, Trained web developers and provide IT support for many businesses and Individuals
+
+</p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="section">
+                      <p>Tech Lead GThree Technologies Limited</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>Training</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
